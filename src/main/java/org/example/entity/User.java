b4a19f554af8bddf5a6020cbfd536d74be239169
oleg-olegi <ol.shink87@gmail.com>
2024-08-06 13:37:01 +0300

    mapper

--- src/main/java/org/example/entity/User.java
+++ src/main/java/org/example/entity/User.java
@@ -11,7 +11,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private int id;
+    private Long id;
     @Column(name = "name")
     private String name;
     @Column(name = "email")
@@ -52,11 +52,11 @@ public class User {
         this.posts = posts;
     }
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 }
